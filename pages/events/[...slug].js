11bfef9b8524b8b5f55c76ecb63870c1bdcbcff7
letanloc <letanlocjp1001@gmail.com>
2023-09-05 09:03:41 +0900

    test

---
+++ pages/events/[...slug].js
@@ -0,0 +1,4 @@
+function FilteredEventsPage() {
+  return <div>FilteredEventsPage</div>
+}
+export default FilteredEventsPage
---
+++ pages/events/index.js
@@ -0,0 +1,4 @@
+function AllEventsPage() {
+  return <div>AllEventsPage</div>
+}
+export default AllEventsPage
